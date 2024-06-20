ad6281864312a446167fbefb9a65c789aff0cb2b
Paga <pagaaverin@gmail.com>
2024-06-20 14:26:37 +0300

    yahaha

---
+++ client/src/page/navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+function Navbar({ user, setUser }) {
+    const onHandleLogout = async () => {
+        const { data } = await requesAxios.get('/auth/logout')
+        if (data.message === 'success') {
+            setAccessToken(undefined)
+            setUser(undefined)
+        }
+    }
+    return (
+        <nav className='Navbar'>
+            <NavLink to='/'>Main</NavLink>
+            <NavLink to='/property'>Property</NavLink>
+            <NavLink to='/favorite'>Favorite</NavLink>
+            {user ? (
+                <button type='button' onClick={onHandleLogout}>
+                    logout
+                </button>
+            ) : (
+                <>
+                    <NavLink to='/registration'>Registration</NavLink>
+                    <NavLink to='/authorization'>Authorization</NavLink>
+                </>
+            )}
+        </nav>
+
+    );
+}
+
+export default Navbar;
