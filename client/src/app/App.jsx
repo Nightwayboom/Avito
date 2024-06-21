b32a50a3a2d027b8688f19b6780d647a1fc45ae5
Paga <pagaaverin@gmail.com>
2024-06-21 14:34:02 +0300

    404

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,51 +1,50 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Authorization from '../page/auth/Authorization';
 import Registration from '../page/auth/Registration';
-import './App.css'
+import './App.css';
 import Property from '../page/property/Property';
 import Favorite from '../page/favorite/Favorite';
 import Navbar from '../page/navbar/Navbar';
 import Main from '../page/main/Main';
+import NotFound from '../page/notfound/NotFound'; // Импортируем компонент NotFound
 import requestAxios, { setAccessToken } from '../services/axios';
 
-
 function App() {
-  const [user, setUser] = useState()
-  const [property, setProperty] = useState()
-  const [favorite, setFavorite] = useState()
+  const [user, setUser] = useState();
+  const [property, setProperty] = useState();
+  const [favorite, setFavorite] = useState();
 
   const axiosProrerty = async () => {
-    const { data } = await requestAxios.get('/property')
+    const { data } = await requestAxios.get('/property');
     if (data.message === 'success') {
-      setProperty(data.property)
+      setProperty(data.property);
     }
-  }
+  };
 
   const axiosFavorite = async () => {
-    const { data } = await requestAxios.get('/favorite')
+    const { data } = await requestAxios.get('/favorite');
     if (data.message === 'success') {
-      setFavorite(data.favorite)
+      setFavorite(data.favorite);
     }
-  }
+  };
 
   const AxiosCheckUser = async () => {
-    const { data } = await requestAxios.get('/tokens/refresh')
+    const { data } = await requestAxios.get('/tokens/refresh');
     if (data.message === 'success') {
-      setUser(data.user)
-      setAccessToken(data.setAccessToken)
+      setUser(data.user);
+      setAccessToken(data.setAccessToken);
     }
-  }
+  };
 
   useEffect(() => {
-    // axiosFavorite()
-    axiosProrerty()
-    AxiosCheckUser()
-  }, [])
+    // axiosFavorite();
+    axiosProrerty();
+    AxiosCheckUser();
+  }, []);
 
   return (
     <>
-
       <div>
         <Navbar user={user} setUser={setUser} />
 
@@ -53,13 +52,11 @@ function App() {
           <Route path='/' element={<Main />} />
           <Route
             path='/property'
-            element={<Property property={property} setProperty={setProperty}user={user}/>}
+            element={<Property property={property} setProperty={setProperty} user={user} />}
           />
           <Route
             path='/favorite'
-            element={
-              <Favorite favorite={favorite} setFavorite={setFavorite} user={user} />
-            }
+            element={<Favorite favorite={favorite} setFavorite={setFavorite} user={user} />}
           />
           <Route
             path='/registration'
@@ -69,16 +66,11 @@ function App() {
             path='/authorization'
             element={<Authorization setUser={setUser} />}
           />
-
-          <Route path='*' element={<h1>404</h1>} />
+          <Route path='*' element={<NotFound />} /> 
         </Routes>
       </div>
-
-
-
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
